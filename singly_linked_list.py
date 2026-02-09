c9cd05f13be9fa0b0b1b63b68eed002c2810c42b
mrymjfri <mrym.jfri1419@gmail.com>
2026-02-10 01:07:36 +0330

    singly_linked_list.py
    لیست پیوندی یک طرفه

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -35,6 +35,7 @@ class linked_list :
                     a = node(y)
                     a.next = c.next
                     c.next = a
+                    return 
                 c = c.next
             print("not found")
 
@@ -54,3 +55,92 @@ class linked_list :
                 return
             C= C.next
         print("not found")
+
+    def del_first(self):
+        if self.head is None:
+            print ("error 0")
+            return 
+        C=self.head
+         self.head=C.next
+         del C
+    def del_last(self):
+        if self.head is None:
+            print ("error 0")
+            return 
+        if self.head.next is None:
+            self.del_firs()
+            return 
+        C=self.head
+        while C.next.next:
+             C=C.next
+         del C.next
+         C.next=None
+    def del_after(self,x):
+        if self.head is None:
+            print ("error 0")
+            return 
+        if self.head.next is None:
+            print ("error 1")
+            self.del_firs()
+            return 
+        C=self.head
+        while C.next.next:
+             if C.data==x:
+                 a=C.next
+                 C.next=a.next
+                  del a
+                  return 
+             C=C.next
+        print ("not found")
+
+    def del_befor(self,x):
+        if self.head is None:
+            print ("error 0")
+            return 
+        if self.head.data ==x:
+            print ("error 1")
+            return 
+        if self.head.next is None:
+            print ("error 2")
+        if self.head.next.data=x:
+            self.del_first()
+            return 
+        if self.head.next.next is None:
+            print ("error 3")
+            return 
+        C=self.head
+        while C.next.next:
+             if C.next.next.data==x:
+                 a=C.next
+                 C.next=a.next
+                  del a
+                  return 
+             C=C.next
+        print ("not found")
+
+    def del_all(self):
+        while self.head:
+            self.del_first()
+    def del_x(self , x):
+        if self.head is None:
+            print ("error 0")
+            return 
+        if self.head.data==x:
+            self.del_firs()
+            return 
+        C=self.head
+        while C.next:
+             if C.next.data==x:
+                 a=C.next
+                 C.next=a.next
+                  del a
+                  return 
+             C=C.next
+        print ("not found")
+
+    def display (self):
+        T=self.head
+         while T !=None:
+            print (T.data , end=" ")
+            T=T.next
+         print ("Null")
