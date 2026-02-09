011e8d1bd4a9b5bbae483bd3c4412b2ebf6e32f3
mrymjfri <mrym.jfri1419@gmail.com>
2026-02-10 01:14:34 +0330

    Create graph.py
    گراف

---
+++ graph.py
@@ -0,0 +1,37 @@
+def BFS(graph , start):
+    queue = [start]
+    visited = {start}
+    while queue:
+        vertex = queue.pop(0)
+        for ne in graph[vertex]:
+            if ne not visited:
+                visited.add(ne)
+                queue.append(ne)
+
+
+    return visited
+
+def DFS(graph , start , visited):
+    visited[start] = True
+    for ne in graph[start]:
+        if not visited[ne]:
+            DFS(graph , ne , visited)
+
+def sort1(A):
+    B = [] *len(A)
+    for j in range(len(A)):
+        min=A[0]
+        K=0
+        for i in range(1,len(A)):
+            if A[j] < min:
+                min = A[j]
+                k = i
+        B[j] = min
+        A[k] = float("lnf")
+    return B 
+
+def Bubble(A):
+    for i in range(len(A)-1):
+        for j in range(len(A)-1):
+            if A[j] > A[j+1]:
+                A[j], A[j+1] = A[j+1] , A[j]
