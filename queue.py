12a4e0f25cd7659c0ae7dc52f80dbae2a3f34fb3
mrymjfri <mrym.jfri1419@gmail.com>
2026-02-10 19:25:54 +0330

    Update queue.py

--- queue.py
+++ queue.py
@@ -1,38 +1,38 @@
 class Queue:
-    def __init__(self ,max = 100):
-        self.list=[None]* max
+    def __init__(self, max=100):
+        self.list = [None] * max
         self.front = -1
         self.rear = -1
-    def insert(self,x):
-        if self.rear >= len(self.list) -1 :
+
+    def insert(self, x):
+        if self.rear >= len(self.list) - 1:
             print("Queue is Full")
             return
-        if  self.front == -1 :
-            self.front+= 1 
-            self.rear+= 1
-            self.list[list.rear] = x
-            return
-        self.rear+= 1 
-        self.list[self.rear] = x
-    def Del_(self):
-        if self.front == -1 :
+        if self.front == -1:
+            self.front = 0
+            self.rear = 0
+            self.list[self.rear] = x
+        else:
+            self.rear += 1
+            self.list[self.rear] = x
+
+    def del_(self):
+        if self.front == -1:
             print("Queue is empty")
-            return     
-        if  self.front == self.rear :
-            k = self.list[self.front]
+            return None
+        k = self.list[self.front]
+        if self.front == self.rear:
             self.front = -1
             self.rear = -1
-            return k
-        k = self.list[self.front]
-        self.front+= 1
+        else:
+            self.front += 1
         return k
-    
 
-
-test = Queue(3)
-test.insert(57)
-test.insert(32)
-test.insert(44)
-test.insert(39) #Queue is Full
-test.Del()
-test.insert(39) #Queue is Full
+    def display(self):
+        if self.front == -1:
+            print("Queue is empty")
+        else:
+            for i in range(self.front, self.rear + 1):
+                print(self.list[i], end=" ")
+            print()
+        
