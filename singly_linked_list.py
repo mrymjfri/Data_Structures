1401971196c52b0fd1f63bf9372b3e1499be3f47
mrymjfri <mrym.jfri1419@gmail.com>
2026-02-10 19:17:44 +0330

    Update singly_linked_list.py

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -1,146 +1,127 @@
-class node :
-    def __init__(self , d):
-        self.Data = d
+class Node:
+    def __init__(self, d):
+        self.data = d
         self.next = None
 
 
-
-
-class linked_list :
+class LinkedList:
     def __init__(self):
         self.head = None
-    def insert_frist(self , x):
-        if self.head == None:
-            self.head = node(x)
-        else:
-            a = node(x)
-            a.next = self.head
-            self.head = a     
-    def insert_last(self , x):
-        if self.head == None:
-            self.head = node(x)
-        else:
-            a = node(x)
-            c = self.head
-            while c.next:
-                c = c.next 
-            c.next = a
-    def insert_after(self , x, y):
-        if self.head is None:
-            print("list is empty")
-        else:
-            c = self.head
-            while c:
-                if c.Data == x:
-                    a = node(y)
-                    a.next = c.next
-                    c.next = a
-                    return 
-                c = c.next
-            print("not found")
 
-    def insert_befor(self , x, y):
+
+
+    def insert_first(self, x):
+        a = Node(x)
+        a.next = self.head
+        self.head = a
+
+    def insert_last(self, x):
+        a = Node(x)
+        if self.head is None:
+            self.head = a
+            return
+        c = self.head
+        while c.next:
+            c = c.next
+        c.next = a
+
+    def insert_after(self, x, y):
+        c = self.head
+        while c:
+            if c.data == x:
+                a = Node(y)
+                a.next = c.next
+                c.next = a
+                return
+            c = c.next
+        print("not found")
+
+    def insert_before(self, x, y):
         if self.head is None:
             print("list is empty")
             return
-        if self.head.Data == x:
-            self.insert_frist(y)
+        if self.head.data == x:
+            self.insert_first(y)
             return
-       C=self.head
+
+        c = self.head
         while c.next:
-            if c.next.Data == x:
-                a = node(y)
+            if c.next.data == x:
+                a = Node(y)
                 a.next = c.next
-                C.next = a
+                c.next = a
                 return
-            C= C.next
+            c = c.next
         print("not found")
+
+    
 
     def del_first(self):
         if self.head is None:
-            print ("error 0")
-            return 
-        C=self.head
-         self.head=C.next
-         del C
+            print("error 0")
+            return
+        self.head = self.head.next
+
     def del_last(self):
         if self.head is None:
-            print ("error 0")
-            return 
+            print("error 0")
+            return
         if self.head.next is None:
-            self.del_firs()
-            return 
-        C=self.head
-        while C.next.next:
-             C=C.next
-         del C.next
-         C.next=None
-    def del_after(self,x):
-        if self.head is None:
-            print ("error 0")
-            return 
-        if self.head.next is None:
-            print ("error 1")
-            self.del_firs()
-            return 
-        C=self.head
-        while C.next.next:
-             if C.data==x:
-                 a=C.next
-                 C.next=a.next
-                  del a
-                  return 
-             C=C.next
-        print ("not found")
-
-    def del_befor(self,x):
-        if self.head is None:
-            print ("error 0")
-            return 
-        if self.head.data ==x:
-            print ("error 1")
-            return 
-        if self.head.next is None:
-            print ("error 2")
-        if self.head.next.data=x:
             self.del_first()
-            return 
-        if self.head.next.next is None:
-            print ("error 3")
-            return 
-        C=self.head
-        while C.next.next:
-             if C.next.next.data==x:
-                 a=C.next
-                 C.next=a.next
-                  del a
-                  return 
-             C=C.next
-        print ("not found")
+            return
+        c = self.head
+        while c.next.next:
+            c = c.next
+        c.next = None
+
+    def del_after(self, x):
+        c = self.head
+        while c and c.next:
+            if c.data == x:
+                c.next = c.next.next
+                return
+            c = c.next
+        print("not found")
+
+    def del_before(self, x):
+        if self.head is None or self.head.next is None:
+            print("error")
+            return
+        if self.head.next.data == x:
+            self.del_first()
+            return
+
+        c = self.head
+        while c.next and c.next.next:
+            if c.next.next.data == x:
+                c.next = c.next.next
+                return
+            c = c.next
+        print("not found")
+
+    def del_x(self, x):
+        if self.head is None:
+            print("error 0")
+            return
+        if self.head.data == x:
+            self.del_first()
+            return
+
+        c = self.head
+        while c.next:
+            if c.next.data == x:
+                c.next = c.next.next
+                return
+            c = c.next
+        print("not found")
 
     def del_all(self):
-        while self.head:
-            self.del_first()
-    def del_x(self , x):
-        if self.head is None:
-            print ("error 0")
-            return 
-        if self.head.data==x:
-            self.del_firs()
-            return 
-        C=self.head
-        while C.next:
-             if C.next.data==x:
-                 a=C.next
-                 C.next=a.next
-                  del a
-                  return 
-             C=C.next
-        print ("not found")
+        self.head = None
 
-    def display (self):
-        T=self.head
-         while T !=None:
-            print (T.data , end=" ")
-            T=T.next
-         print ("Null")
+
+    def display(self):
+        t = self.head
+        while t:
+            print(t.data, end=" -> ")
+            t = t.next
+        print("None")
