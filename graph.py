9c841dd2071a55da014a5b042d6c95ef936b78d1
mrymjfri <mrym.jfri1419@gmail.com>
2026-02-18 17:04:07 +0330

    Update graph.py

--- graph.py
+++ graph.py
@@ -26,15 +26,15 @@ def sort1(A):
     B = [0] * len(A)
 
     for j in range(len(A)):
-        min = A[0]
+        min_val = A[0]
         k = 0
 
         for i in range(1, len(A)):
-            if A[i] < min:
-                min = A[i]
+            if A[i] < min_val:
+                min_val = A[i]
                 k = i
 
-        B[j] = min
+        B[j] = min_val
         A[k] = float("inf")
 
     return B
